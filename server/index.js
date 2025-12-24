0fc36e318dcde9788b706abe30a64b61a6259369
saurabhborikar13 <saurabhborikar13@gmail.com>
2025-12-24 20:34:02 +0530

    case-sensitive-error-resolve

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const authRoutes = require('./routes/auth');
 
 // Import routes
-const jobRoutes = require('./routes/jobs');
+const jobRoutes = require('./routes/jobRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/Jobs', jobRoutes);
+app.use('/api/jobRoutes', jobRoutes);
 app.use('/api/auth', authRoutes); 
 
 
