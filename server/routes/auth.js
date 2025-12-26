599046986b0cff4d512f524db7164ce66b7ce1ee
saurabhborikar13 <saurabhborikar13@gmail.com>
2025-12-26 22:34:24 +0530

    naming bug fixed

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { login, register } = require('../controllers/authController');
+const authenticateUser = require('../middleware/authentication');
 
 router.post('/register', register);
 router.post('/login', login);
